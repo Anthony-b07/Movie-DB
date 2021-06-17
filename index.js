1684612d4a31521c3467f7fc285fc4efa5093546
Anthony <karam.antho@gmail.com>
2021-06-17 18:12:20 +0300

    step3

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("ok");
-});
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+app.get("/test", (req, res) => {
+  res.send(`{ status: 200, message: "ok" }`);
+});
+app.get("/time", (req, res) => {
+  res.send(`{status:200, message:${TIME}}`);
+});
+let date = new Date();
+let TIME = date.getHours() + ":" + date.getMinutes();
